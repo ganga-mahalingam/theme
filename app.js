db6cb4bad47bb85e2751e02b9e98833eb59cb674
Ganga Mahalingam <gangamahalingam19@gmail.com>
2025-03-19 16:08:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var body=document.querySelector("body");
+function dark_theme(){
+    body.className="";
+    body.classList.add("dark");
+}
+
+function light_theme(){
+    body.className="";
+    body.classList.add("light");
+
+}
